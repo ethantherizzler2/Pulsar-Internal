5ee0b0b2f1008c66c25bf205d71e77c614eac44a
Ethan <ethandiddy33@gmail.com>
2026-02-21 18:36:34 +0100

    Remove unnecessary newline and clean up code

--- module/loader.cpp
+++ module/loader.cpp
@@ -46,5 +46,3 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     }
     return TRUE;
 }
-
-extern "C" __declspec(dllexport) int NextHook(int nCode, WPARAM wParam, LPARAM lParam) { return CallNextHookEx(NULL, nCode, wParam, lParam); }
